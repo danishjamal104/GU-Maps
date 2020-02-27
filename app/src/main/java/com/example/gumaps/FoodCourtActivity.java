a4f4be1051441b4114e3a0d9ece4bb7fc0298388
DANISH <danishjamal.104@gmail.com>
2020-02-28 03:02:58 +0530

    All basic activities added

---
+++ app/src/main/java/com/example/gumaps/FoodCourtActivity.java
@@ -0,0 +1,14 @@
+package com.example.gumaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodCourtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_court);
+    }
+}
